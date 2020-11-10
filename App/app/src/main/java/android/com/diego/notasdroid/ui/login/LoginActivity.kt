a198673a8a49bce3a5fcebf5cad99586b7ff3aa9
Diego Sanchez <diegozj92@gmail.com>
2020-11-10 18:18:41 +0100

    Interfaz Login
    Realizada Interfaz Login

--- App/app/src/main/java/android/com/diego/notasdroid/ui/login/LoginActivity.kt
+++ App/app/src/main/java/android/com/diego/notasdroid/ui/login/LoginActivity.kt
@@ -26,9 +26,9 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_login)
 
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
+        val username = findViewById<EditText>(R.id.textUser_Login)
+        val password = findViewById<EditText>(R.id.textPwd_Login)
+        val login = findViewById<Button>(R.id.btnSignIn_Login)
         val loading = findViewById<ProgressBar>(R.id.loading)
 
         loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
