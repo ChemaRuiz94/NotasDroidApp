0aa9aa0e8fc50bdf1ef74457de506b57e5a076f6
Diego Sanchez <diegozj92@gmail.com>
2020-11-10 18:05:37 +0100

    Creacion Proyecto

---
+++ App/app/src/main/java/android/com/diego/notasdroid/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package android.com.diego.notasdroid.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
