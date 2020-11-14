419c482e91c6049e94766445c4c4121c77e4dc7b
Diego Sanchez <diegozj92@gmail.com>
2020-11-14 19:53:18 +0100

    gitflow-feature-stash: BBDD

---
+++ App/app/src/main/java/android/com/diego/notasdroid/datos/UsersController.kt
@@ -0,0 +1,62 @@
+package android.com.diego.notasdroid.datos
+
+import android.content.Context
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import io.realm.kotlin.where
+
+object UsersController {
+
+    private const val DATOS_BD = "DATOS_BD_REALM"
+    private const val DATOS_BD_VERSION = 1L
+
+    fun initRealm(context: Context?){
+        Realm.init(context)
+        val config = RealmConfiguration.Builder()
+            .name(DATOS_BD)
+            .schemaVersion(DATOS_BD_VERSION)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(config)
+        Log.d("Datos", "Iniciando Realm")
+    }
+
+    fun selectDatos(): MutableList<User>?{
+        return Realm.getDefaultInstance().copyFromRealm(
+            Realm.getDefaultInstance().where<User>().findAll()
+        )
+    }
+
+    fun selectDatoByEmail(email : String) : User? {
+
+        return Realm.getDefaultInstance().copyFromRealm(
+            Realm.getDefaultInstance().where<User>().equalTo("email", email).findFirst()
+        )
+
+    }
+
+    fun insertDato(user: User){
+        Realm.getDefaultInstance().executeTransaction{
+            it.copyToRealm(user)
+        }
+    }
+
+    fun deleteDato(user: User){
+        Realm.getDefaultInstance().executeTransaction{
+            it.where<User>().equalTo("email", user.email).findFirst()?.deleteFromRealm()
+        }
+    }
+
+    fun updateDato(user: User){
+        Realm.getDefaultInstance().executeTransaction{
+            it.copyToRealmOrUpdate(user)
+        }
+    }
+
+    fun removeAll(){
+        Realm.getDefaultInstance().executeTransaction{
+            it.deleteAll()
+        }
+    }
+}
